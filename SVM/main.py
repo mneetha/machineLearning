7f9191eb48eb3bf730e0404d6fa60e13669e4ce9
Neetha Menon <neets.86@gmail.com>
2025-01-29 11:50:16 -0800

    Support Vector machine

---
+++ SVM/main.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection  import train_test_split
+from sklearn.svm import SVC
+
+
+data = load_breast_cancer()
+
+x = data.data
+y= data.target
+
+x_train, x_test, y_train, y_test = train_test_split(x,y, test_size=0.2)
+
+clf = SVC(kernel='linear', C=3)
+clf.fit(x_train, y_train)
+print(f'SVC: {clf.score(x_test, y_test)}')
+
+clf2 = KNeighborsClassifier(n_neighbors=3)
+clf2.fit(x_train, y_train)
+print(f'KNN: {clf2.score(x_test, y_test)}')
