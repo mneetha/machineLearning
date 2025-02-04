0251c2e1231afd210b80cd0d4f6c84c2c22b9f55
Neetha Menon <neets.86@gmail.com>
2025-02-04 00:41:53 -0800

    data preprocessing

--- HousePricing/main.py
+++ HousePricing/main.py
@@ -12,3 +12,17 @@ y = data['median_house_value']
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 
 train_data = X_train.join(y_train)
+
+#Data preprocessing
+train_data['total_rooms'] = np.log(train_data['total_rooms'] + 1)
+train_data['total_bedrooms'] = np.log(train_data['total_bedrooms'] + 1)
+train_data['population'] = np.log(train_data['population'] + 1)
+train_data['households'] = np.log(train_data['households'] + 1)
+
+# plt.figure(figsize=(15,8))
+# sns.heatmap(train_data.corr(), annot=True, cmap="YlGnBu")
+# train_data.ocean_proximity.value_counts()
+train_data.join(pd.get_dummies(train_data.ocean_proximity)).drop(['ocean_proximity'], axis=1)
+
+# plt.figure(figsize=(15,8))
+# sns.scatterplot(x="latitude", y="longitude", data=train_data, hue="median_house_value", palette="coolwarm")
